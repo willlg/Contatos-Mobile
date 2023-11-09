2b79c63b2d9f4a798756232dff0052c387566e42
willlg <you@example.com>
2023-11-09 08:59:54 -0300

    começo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDSAFUIMUU0rpMNRv87-dMx1X_7F0vDvkw",
+
+    authDomain: "contatos-mobile-528f7.firebaseapp.com",
+  
+    projectId: "contatos-mobile-528f7",
+  
+    storageBucket: "contatos-mobile-528f7.appspot.com",
+  
+    messagingSenderId: "1089569138939",
+  
+    appId: "1:1089569138939:web:f22a69a01398dd345fe85e"
+  }
+};
