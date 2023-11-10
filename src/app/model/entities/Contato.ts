9a9fad577e9f2be8dfe70325d4c695b5c5a19553
willlg <you@example.com>
2023-11-10 11:26:36 -0300

    imagens funcionando

--- src/app/model/entities/Contato.ts
+++ src/app/model/entities/Contato.ts
@@ -2,6 +2,7 @@ export default class Contato{
   private _id: string;
   private _nome: string;
   private _telefone: number;
+  private _downloadURL: any;
 
   constructor(nome: string, telefone: number){
     this._nome = nome;
@@ -28,6 +29,12 @@ export default class Contato{
     this._telefone = value;
   }
 
-
+  public get downloadURL(): any {
+    return this._downloadURL;
+  }
+  
+  public set downloadURL(value: any) {
+    this._downloadURL = value;
+  }
 
 }
--- src/app/model/services/firebase.service.ts
+++ src/app/model/services/firebase.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import Contato from '../entities/Contato';
+import { finalize } from 'rxjs/operators'
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,7 @@ import Contato from '../entities/Contato';
 export class FirebaseService {
   private PATH : string = 'contatos';
 
-  constructor(private firestore : AngularFirestore) { }
+  constructor(private firestore : AngularFirestore, private storage: AngularFireStorage) { }
 
   read(){
     return this.firestore.collection(this.PATH)
@@ -20,14 +22,48 @@ export class FirebaseService {
     .add({nome: contato.nome, telefone: contato.telefone});
   }
 
+  createWithImage(contato: Contato){
+    return this.firestore.collection(this.PATH)
+    .add({nome: contato.nome, telefone: contato.telefone, downloadURL : contato.downloadURL});
+  }
+
   update(contato: Contato, id: string){
     return this.firestore.collection(this.PATH).doc(id)
     .update({nome: contato.nome, telefone: contato.telefone});
+  }
+
+  updateWithImage(contato: Contato, id: string){
+    return this.firestore.collection(this.PATH).doc(id)
+    .update({nome: contato.nome, telefone: contato.telefone, downloadURL : contato.downloadURL});
   }
 
   delete(contato: Contato){
     return this.firestore.collection(this.PATH)
     .doc(contato.id)
     .delete()
+  }
+
+  uploadImage(imagem: any, contato: Contato){
+    const file = imagem.item(0);
+    if(file.type.split('/')[0] !== 'image'){
+      console.error('Tipo Não Suportado');
+      return;
+    }
+    const path = `images/${contato.nome}_${file.name}`;
+    const fileRef = this.storage.ref(path);
+    let task = this.storage.upload(path, file);
+    task.snapshotChanges().pipe(
+    finalize(()=>{
+      let uploadFileURL = fileRef.getDownloadURL();
+      uploadFileURL.subscribe(resp=>{
+        contato.downloadURL = resp;
+        if(!contato.id){
+          this.createWithImage(contato);
+        }else{
+          this.updateWithImage(contato, contato.id);
+        }
+      })
+    })
+  ).subscribe();
   }
 }
